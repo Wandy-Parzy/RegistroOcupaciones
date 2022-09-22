aa6b23a58d31e8b396b5103a8078cda8d742d470
Wandy-Parzy <112360996+Wandy-Parzy@users.noreply.github.com>
2022-09-22 19:09:13 -0400

    Agregando registro de personas
    Agregando registro de personas

---
+++ Registro/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Registro.Model;
+
+namespace Registro.DAL{
+public class Contexto : DbContext
+{
+    public DbSet<Ocupaciones> Ocupaciones { get; set; }
+
+    public DbSet<Persona> Persona { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+}
+}
+ 
